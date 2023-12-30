0dd6f26a4fa0b3b71a8ab79a5c29a96480a58af4
opuopu <nurmdopu428@gmail.com>
2023-12-30 12:15:20 +0600

    TB-4 : added parseData middleware

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,18 +1,15 @@
 import nodemailer from "nodemailer";
 import config from "../config/index.js";
-
 const transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 465,
   secure: true,
   auth: {
-    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
     user: config.nodemailer_host_email,
     pass: config.nodemailer_host_pass,
   },
 });
 async function sendEmail(receiverGmail, code, subject, text, children) {
-  // send mail with defined transport object
   try {
     const info = await transporter.sendMail({
       from: `tidy bayti adminv ${config.nodemailer_host_email}`, // sender address
