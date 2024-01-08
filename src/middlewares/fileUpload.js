978ae68fa59efd09c2174dda2926fa725057b907
opuopu <nurmdopu428@gmail.com>
2024-01-08 10:21:15 +0600

    refactor: refactor fileupload middleware

---
+++ src/middlewares/fileUpload.js
@@ -0,0 +1,30 @@
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "/public/upload/profile/");
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+const upload = {
+  storage: storage,
+  limits: {
+    fileSize: 2000000,
+  },
+  fileFilter: function (req, file, cb) {
+    if (
+      file.mimetype === "image/png" ||
+      file.mimetype === "image/jpg" ||
+      file.mimetype === "image/jpeg"
+    ) {
+      cb(null, true);
+    } else {
+      cb(new Error("only png,jpg,jpeg format allowed"), false);
+    }
+  },
+};
+export default upload;
