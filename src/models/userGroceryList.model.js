f49b743e0e3a719d5752cfba54170eca7077a9a5
opuopu <nurmdopu428@gmail.com>
2024-01-10 14:33:23 +0600

    feat: created usergrocery model

---
+++ src/models/userGroceryList.model.js
@@ -0,0 +1,33 @@
+import { Schema, Types, model } from "mongoose";
+
+const userGrocerySchema = new Schema(
+  {
+    homeOwner: {
+      type: Types.ObjectId,
+      ref: "HomeOwner",
+    },
+    assignedEmployee: {
+      type: String,
+      ref: "Employee",
+    },
+    groceryLists: [
+      {
+        type: Types.ObjectId,
+        ref: "GroceryList",
+      },
+    ],
+    date: {
+      type: Date,
+    },
+    completed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const UserGroceryList = model("UserGroceryList", userGrocerySchema);
+export default UserGroceryList;
