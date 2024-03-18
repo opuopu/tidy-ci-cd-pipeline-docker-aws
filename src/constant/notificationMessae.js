a0acecd4d042b68b574ccb52c9450fc35520ab09
opuopu <nurmdopu428@gmail.com>
2024-03-18 15:45:02 +0600

    feat: added employees api

--- src/constant/notificationMessae.js
+++ src/constant/notificationMessae.js
@@ -1,4 +1,6 @@
 export const TaskNotifcationMessage = {
+  buyRequest: "your employee sent a grocery buy request",
+  acceptBuyRequest: "your request was accepted",
   daily: "you have new daily task assigned",
   grocery: "You have a new grocery task assigned.",
   busy: "Employee is busy, unable to take tasks.",
