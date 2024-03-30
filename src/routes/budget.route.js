4a8708d93d893bb7fff6f611d0a692b043b94d03
opuopu <nurmdopu428@gmail.com>
2024-03-30 16:30:02 +0600

    feat:added zod validation in budget post route

--- src/routes/budget.route.js
+++ src/routes/budget.route.js
@@ -2,9 +2,12 @@ import express from "express";
 import auth from "../middlewares/auth.js";
 import { USER_ROLE } from "../constant/user.role.js";
 import budgetControllers from "../controllers/budget.controller.js";
+import validateRequest from "../middlewares/validateRequest.js";
+import budgetValidationSchema from "../validation/budget.validation.js";
 const router = express.Router();
 router.post(
   "/",
+  validateRequest(budgetValidationSchema.postBudgetSchema),
   auth(USER_ROLE.HOMEOWNER),
   budgetControllers.insertBudgetIntoDB
 );
--- src/validation/budget.validation.js
+++ src/validation/budget.validation.js
@@ -1,18 +1,28 @@
+import dayjs from "dayjs";
 import { z } from "zod";
 
-const budgetSchema = z.object({
+const postBudgetSchema = z.object({
   body: z
     .object({
       category: z.string({ required_error: "category is required" }),
       amountType: z.string({ required_error: "amount type is required" }),
       amount: z.string({ required_error: "amount is required" }),
+      month: z.string({ required_error: "amount is required" }),
     })
     .refine(
       (body) => {
-        console.log(body);
+        const [year, mon] = body?.month?.split("-");
+        console.log(year, mon);
+        const checkDigits = mon.length >= 2;
+        return checkDigits;
       },
       {
-        message: "New Password and Confirm Password does not match",
+        required_error: "please select valid date",
       }
     ),
 });
+
+const budgetValidationSchema = {
+  postBudgetSchema,
+};
+export default budgetValidationSchema;
